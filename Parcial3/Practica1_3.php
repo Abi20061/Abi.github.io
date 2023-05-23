18f4cd6992a64f49819777edfdabb16b7098afc5
Abi20061 <124410675+Abi20061@users.noreply.github.com>
2023-05-22 17:58:30 -0700

    Add files via upload
    22/05/2023 practica con php "hOLA MUNDO2

---
+++ Parcial3/Practica1_3.php
@@ -0,0 +1,23 @@
+<?php
+
+    echo "Hola Mundo";
+
+    echo "<hr>";
+
+    $nombre = "Abi";
+    $edad = 17;
+    
+    if($edad >= 17){
+        echo $nombre . " es mayor de edad";
+    } else{
+        echo $nombre . " es menor de edad";
+    }
+    
+        echo "<br><br>";
+
+    for($i=0; $i<10; $i++){
+        echo $i . "<br>";
+    }
+
+
+?>
