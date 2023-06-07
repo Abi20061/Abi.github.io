c562876f30d9cd39acae76923d4d69217a0d3c3e
Abi20061 <124410675+Abi20061@users.noreply.github.com>
2023-06-06 17:40:37 -0700

    Add files via upload
    CONTINUACION JUEGO DEL GATITO:))

--- Parcial3/Practica5.php
+++ Parcial3/Practica5.php
@@ -1,6 +1,24 @@
 <?php
 
     $cuadros = $_POST["cuadro"];
-    var_dump($cuadros);
+   
+
+    for($i=0; $i<count($cuadros); $i++){
+        echo $cuadros[$i]. ",";
+        if($cuadros[$i] != 'X' && $cuadros[$i] =! "0"){
+            echo "<h2>Solo se permiten  X y/o O ---> </h2>";
+            return;
+        }
+    }
+
+    if($cuadros[0] == "X" && $cuadros[1] == "X" && $cuadros[2] == "X"){
+        echo "<h1>Ganador X</h1>";
+        return;
+    }else if($cuadros[0] == "O" && $cuadros[1] == "O" && $cuadros[2] == "O"){
+        echo "<h1>Ganador [O]</h1>";
+
+    }
+
+
 
 ?>
